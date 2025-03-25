2cf83127e4a3a028173361bc23a867712372a23b
eduardodelalastra <eduardodelalastra@guadaltel.com>
2025-03-25 14:00:45 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ow-Proyecto2-nostandalone',
+  webDir: 'www'
+};
+
+export default config;
